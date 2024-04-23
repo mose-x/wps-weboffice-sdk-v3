7e78aa8965959394850818b032b194a0c54b1063
mose <mose-x@qq.com>
2024-04-24 00:54:39 +0800

    增加文件注释以及增加官方解释入口，修改版本号（不重要）

--- src/main/java/cn/ljserver/tool/weboffice/v3/config/AutoConfig.java
+++ src/main/java/cn/ljserver/tool/weboffice/v3/config/AutoConfig.java
@@ -25,22 +25,29 @@ public class AutoConfig {
     @PostConstruct
     public void validateProperty() {
         Logger log = Logger.getLogger(this.getClass().getName());
+        if (webOfficeProperties.getConvert() == null) {
+            log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.convert' is null.");
+        } else {
+            if (webOfficeProperties.getConvert().getAppid() == null || webOfficeProperties.getConvert().getAppid().isEmpty()) {
+                log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.convert.appid' is null or empty.");
+            }
+            if (webOfficeProperties.getConvert().getSecret() == null || webOfficeProperties.getConvert().getSecret().isEmpty()) {
+                log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.convert.secret' is null or empty.");
+            }
+            if (webOfficeProperties.getConvert().getDomain() == null || webOfficeProperties.getConvert().getDomain().isEmpty()) {
+                log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.convert.domain' is null or empty.");
+            }
+        }
 
-        if (webOfficeProperties.getConvert().getAppid() == null || webOfficeProperties.getConvert().getAppid().isEmpty()) {
-            log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.convert.appid' is null or empty.");
-        }
-        if (webOfficeProperties.getConvert().getSecret() == null || webOfficeProperties.getConvert().getSecret().isEmpty()) {
-            log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.convert.secret' is null or empty.");
-        }
-        if (webOfficeProperties.getConvert().getDomain() == null || webOfficeProperties.getConvert().getDomain().isEmpty()) {
-            log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.convert.domain' is null or empty.");
-        }
-
-        if (webOfficeProperties.getPreview().getAppid() == null || webOfficeProperties.getPreview().getAppid().isEmpty()) {
-            log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.preview.appid' is null or empty.");
-        }
-        if (webOfficeProperties.getPreview().getSecret() == null || webOfficeProperties.getPreview().getSecret().isEmpty()) {
-            log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.preview.secret' is null or empty.");
+        if (webOfficeProperties.getPreview() == null) {
+            log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.preview' is null.");
+        } else {
+            if (webOfficeProperties.getPreview().getAppid() == null || webOfficeProperties.getPreview().getAppid().isEmpty()) {
+                log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.preview.appid' is null or empty.");
+            }
+            if (webOfficeProperties.getPreview().getSecret() == null || webOfficeProperties.getPreview().getSecret().isEmpty()) {
+                log.log(Level.WARNING, "web-office-v3: WARNING: Required application property 'web-office.preview.secret' is null or empty.");
+            }
         }
     }
 }
