e2ae2dc58328bfda502213fc4b7a8cd750c7aa19
ming <mose-x@qq.com>
2024-04-28 14:50:56 +0800

    ImgToDoc

--- src/main/java/cn/ljserver/tool/weboffice/v3/model/convert/ImgToDocRequest.java
+++ src/main/java/cn/ljserver/tool/weboffice/v3/model/convert/ImgToDocRequest.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sun.org.glassfish.gmbal.Description;
 import lombok.*;
 
+import java.util.List;
+
 /**
  * img转document请求参数
  */
@@ -26,4 +28,12 @@ public class ImgToDocRequest {
     @JsonProperty("sheet_option")
     @Description("转换为表格时配置：sheet转换方式 0表示每页pdf(每页图片)一个sheet, 1表示所有页面（图片）转到一个sheet中， 默认为0")
     private Integer sheetOption = 0;
+
+    public ImgToDocRequest(@NonNull String[] imgUrls) {
+        this.imgUrls = imgUrls;
+    }
+
+    public ImgToDocRequest(@NonNull List<String> imgUrls) {
+        this.imgUrls = imgUrls.toArray(new String[]{});
+    }
 }
--- src/main/java/cn/ljserver/tool/weboffice/v3/service/convert/toDoc/ImgToDoc.java
+++ src/main/java/cn/ljserver/tool/weboffice/v3/service/convert/toDoc/ImgToDoc.java
@@ -5,14 +5,24 @@ import cn.ljserver.tool.weboffice.v3.model.convert.ConvertResponse;
 import cn.ljserver.tool.weboffice.v3.model.convert.ImgToDocRequest;
 import cn.ljserver.tool.weboffice.v3.util.ConvertUtils;
 import cn.ljserver.tool.weboffice.v3.util.FileUtils;
-import lombok.SneakyThrows;
+
+import java.util.List;
 
 public class ImgToDoc {
-    @SneakyThrows
     public static ConvertResponse convert(String officeType, ImgToDocRequest r) {
         FileUtils.typeMatchCheck(FileUtils.convertToDocumentTypes, officeType);
         if (r.getImgUrls() == null || r.getImgUrls().length == 0) throw new InvalidArgument();
         String uri = "/api/developer/v1/office/img/convert/to/" + officeType.toLowerCase();
         return ConvertUtils.post(uri, r, ConvertResponse.class);
+    }
+
+    public static ConvertResponse convert(String officeType, String[] url) {
+        if (url == null || url.length == 0) throw new InvalidArgument();
+        return convert(officeType, new ImgToDocRequest(url));
+    }
+
+    public static ConvertResponse convert(String officeType, List<String> url) {
+        if (url == null || url.isEmpty()) throw new InvalidArgument();
+        return convert(officeType, new ImgToDocRequest(url));
     }
 }
