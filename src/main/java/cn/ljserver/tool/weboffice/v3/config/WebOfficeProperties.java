c3147d86912fc08ec252835bfab9840eb867ffd9
ming <mose-x@qq.com>
2024-04-30 12:45:42 +0800

    增加让接入端提示配置文件的pom

--- src/main/java/cn/ljserver/tool/weboffice/v3/config/WebOfficeProperties.java
+++ src/main/java/cn/ljserver/tool/weboffice/v3/config/WebOfficeProperties.java
@@ -1,11 +1,15 @@
 package cn.ljserver.tool.weboffice.v3.config;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 开放平台配置对象
  */
+@Data
 @Configuration
 @ConfigurationProperties("web-office")
 public class WebOfficeProperties {
@@ -13,63 +17,27 @@ public class WebOfficeProperties {
     /**
      * 转换配置
      */
+    @NestedConfigurationProperty
     private ConvertConfig convert;
 
     /**
      * 预览配置
      */
+    @NestedConfigurationProperty
     private Config preview;
 
-    public Config getPreview() {
-        return preview;
-    }
-
-    public void setPreview(Config preview) {
-        this.preview = preview;
-    }
-
-    public ConvertConfig getConvert() {
-        return convert;
-    }
-
-    public void setConvert(ConvertConfig convert) {
-        this.convert = convert;
-    }
-
-
+    @Data
     public static class Config {
 
         private String appid;
         private String secret;
 
-        public String getAppid() {
-            return appid;
-        }
-
-        public void setAppid(String appid) {
-            this.appid = appid;
-        }
-
-        public String getSecret() {
-            return secret;
-        }
-
-        public void setSecret(String secret) {
-            this.secret = secret;
-        }
     }
 
-
+    @Data
+    @EqualsAndHashCode(callSuper = true)
     public static class ConvertConfig extends Config {
         private String domain = "https://solution.wps.cn";
-
-        public String getDomain() {
-            return domain;
-        }
-
-        public void setDomain(String domain) {
-            this.domain = domain;
-        }
     }
 
 }
